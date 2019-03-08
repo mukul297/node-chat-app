15a186a8c5823006372f8234bfeecf4d96dd10ac
EC2 Default User <ec2-user@ip-172-31-43-180.us-east-2.compute.internal>
2019-03-08 11:06:55 +0000

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 var path = require("path");
+var http = require('http');
 var express = require("express");
-var app = express();
+var socketIO = require('socket.io');
+
+
+
 var publicPath = path.join(__dirname,"/../public");
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT,process.env.IP,function(){
+io.on('connection',function(socket){
+    console.log("New User Connected");
+    
+    socket.on('disconnect',function(){
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(process.env.PORT,process.env.IP,function(){
     console.log("Server is Started");
 });
 

6f056639ce398f3c5f419b088c3e1070be80d923
EC2 Default User <ec2-user@ip-172-31-43-180.us-east-2.compute.internal>
2019-03-08 12:06:25 +0000

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -16,14 +16,15 @@ app.use(express.static(publicPath));
 io.on('connection',function(socket){
     console.log("New User Connected");
     
-    socket.emit('newMessage',{
-        from : 'John',
-        text : 'Well See you then.',
-        createdAt: 123123
-    });
     
     socket.on('createMessage',function(message){
         console.log('createMessage',message);
+        
+        io.emit('newMessage',{
+          from : message.from,
+          text : message.text,
+          createdAt: new Date().getTime()
+        });
     });
     
     socket.on('disconnect',function(){
